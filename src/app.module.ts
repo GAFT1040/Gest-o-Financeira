60c5ab15c1df4862780f787fcc725438aaa49657
Gabriel Ferrari <gabrielferrarisan@gmail.com>
2025-06-26 20:33:29 -0300

    chore: Configuração do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const logger = new Logger('TypeOrmModule');
+        const hostname = config.get<string>('DB_HOST') || 'localhost';
+        const port = config.get<number>('DB_PORT') || 5432;
+        const user = config.get<string>('DB_USER');
+        const password = config.get<string>('DB_PASS');
+
+        if (!hostname || !port || !user || !password) {
+          logger.error('Verifique as variáveis de ambiente.');
+          process.exit(1);
+        }
+        return {
+          type: 'postgres',
+          host: hostname,
+          port,
+          username: user,
+          password: password,
+          database: 'finance',
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
